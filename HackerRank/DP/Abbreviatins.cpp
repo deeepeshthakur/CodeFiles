ce9ae1f6ca5a241f7c068cb2c5b9b731d3c3badd
deeepeshthakur <deeepeshthakur@gmail.com>
2018-12-07 12:28:46 +0530

    Added Abbreviations Codefile

--- HackerRank/DP/Abbreviatins.cpp
+++ HackerRank/DP/Abbreviatins.cpp
@@ -1,127 +1,74 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int coun = 0, previ = 0;
 
-// std::string reduce( std::string a)
-// {
-//     std::string temp {};
+bool dp(int i, int j, std::vector < std::vector < bool > >& dpTable, std::string& aStr, std::string& bStr){
+	if(i && j){
+		if(aStr[i - 1] >= 'a' && aStr[i - 1] <= 'z'){
+			if(dpTable[i - 1][j])
+				return true;
+		}
 
-// }
+		if((aStr[i - 1] - 'a' == bStr[j - 1] - 'A' || aStr[i - 1] - 'A' == bStr[j - 1] - 'A') && dpTable[i - 1][j - 1])
+			return true;
 
-bool alp_check( char a, char b)
-{
-    bool ans = false;
+		return false;
+	}
+	else{
+		if(i == 0 && j == 0){
+			return true;
+		}
 
-    if(a - 'a' == b - 'A')
-        ans = true;
-    return ans;
+		if(i == 0 && j > 0){
+			return false;
+		}
+
+		if(j == 0 && i > 0){
+			if(aStr[i - 1] <= 'z' && aStr[i - 1] >= 'a' && dpTable[i - 1][0]){
+				return true;
+			}
+			return false;
+		}
+	}
+	return false;
 }
 
-bool check(std::string &a, std::string &b, int a_index, int b_index)
-{
-    coun++;
-    if(coun > 10000)
-        return false;
+void f(){
+	std::string aStr{}, bStr{};
+	std::cin >> aStr >> bStr;
+	std::vector < std::vector < bool > > dpTable( aStr.length() + 1, std::vector < bool >(bStr.length() + 1, false));
+	for(int x = 0; x < dpTable.size(); x++){
+		for(int y = 0; y < dpTable[0].size(); y++){
+			dpTable[x][y] = dp( x, y, dpTable, aStr, bStr);
+		}
+	}
 
-    //std::cout << a_index << " " << b_index << "\n";
-    bool ans;
-    if(b_index == b.length() || a_index == a.length())
-        {
-            if(b_index == b.length())
-                {
-                    ans = true;
-                    for( int index = a_index; index < a.length(); index++)
-                        if(a[index] >= 'A' && a[index] <= 'Z')
-                        {
-                            ans = false;
-                        }
-                }
-            else
-                ans = false;
-        }
-    else
-    {
-        if(a[a_index] >= 'A' && a[a_index] <= 'Z')
-        {
-            if(a[a_index] == b[b_index])
-            {
-                previ = a_index;
-                ans = check( a, b, a_index + 1, b_index + 1);
-            }
-            else
-            {
-                //std::cout << a_index << " ";
-                if(a[previ] >= 'a' && a[previ] <= 'z')
-                {
-                    if((a[previ] - 'a' == a[a_index] - 'A'))
-                    {
-                        previ = a_index;
-                        ans = check( a, b, a_index + 1, b_index);
-                    }
-                    else
-                    {
-                        if(a[a_index] == b[0])
-                            {
-                                previ = a_index;
-                                ans = check( a, b, a_index + 1, 1);
-                            }
-                        else
-                            ans = check( a, b, a_index + 1, 0);
-                    }
-                }
-                else
-                {
-                    if(a[a_index] == b[0])
-                        {
-                            previ = a_index;
-                            ans = check( a, b, a_index + 1, 1);
-                        }
-                    else
-                        ans = check( a, b, a_index + 1, 0);
-                }
-            }
-        }
-        else
-        {
-            if(alp_check( a[a_index], b[b_index]))
-            {
-                previ = a_index;
-                ans = check( a, b, a_index+1, b_index+1);
-            }
-            else
-            {
-                ans = check( a, b, a_index+1, b_index);
-            }
-        }
-    }
+	// for(int x = 0; x < dpTable.size(); x++){
+	// 	for(int y = 0; y < dpTable[0].size(); y++){
+	// 		if(dpTable[x][y]){
+	// 			std::cout << "1 ";
+	// 		}
+	// 		else{
+	// 			std::cout << "0 ";
+	// 		}
+	// 	}
+	// 	std::cout << "\n";
+	// }
 
-    return ans;
+	if(dpTable[dpTable.size() - 1][dpTable[0].size() - 1]){
+		std::cout << "YES\n";
+	}
+	else{
+		std::cout << "NO\n";
+	}
 }
 
-std::string abbreviation(std::string a, std::string b) {
-    // Complete this function
-    std::string ans{};
-    previ = 0;
-    // a = reduce(a);
-    if(check( a, b, 0, 0))
-        ans = "YES";
-    else
-        ans = "NO";
-    coun = 0;
-    return ans;
-}
 
 int main() {
     int q;
     std::cin >> q;
-    for(int a0 = 0; a0 < q; a0++){
-        std::string a;
-        std::cin >> a;
-        std::string b;
-        std::cin >> b;
-        std::string result = abbreviation(a, b);
-        std::cout << result << std::endl;
+    while(q--){
+    	f();
     }
     return 0;
 }
