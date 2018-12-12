7364c5597cb66eca0de76ad07a3b11d62f659376
deeepeshthakur <deeepeshthakur@gmail.com>
2018-12-12 11:36:27 +0530

    Second Trail of the same question

--- CodeChef/DecemberLongChallenge/BeforeHavingDonuts.cpp
+++ CodeChef/DecemberLongChallenge/BeforeHavingDonuts.cpp
@@ -29,65 +29,84 @@ int main(){
 		std::cin >> tori[i].x >> tori[i].y >> tori[i].z >> tori[i].R >> tori[i].r;
 	}
 
-	for(int k = 0; k < 1; k++)
+	for(int i = 0; i < n; i++)
 	{
-		double xs = 0, xl = 0, ys = 0, yl = 0, zs = 0, zl = 0;
-		for(int i = 0; i < n; i++){
-			if(zs > tori[i].z - tori[i].r){
-				zs = tori[i].z - tori[i].r;
-			}
+		// double xs = 0, xl = 0, ys = 0, yl = 0, zs = 0, zl = 0;
+		// for(int i = 0; i < n; i++){
+		// 	if(zs > tori[i].z - tori[i].r){
+		// 		zs = tori[i].z - tori[i].r;
+		// 	}
 
-			if(zl < tori[i].z + tori[i].r){
-				zl = tori[i].z + tori[i].r;
-			}
+		// 	if(zl < tori[i].z + tori[i].r){
+		// 		zl = tori[i].z + tori[i].r;
+		// 	}
 
-			if(xs > tori[i].x - (tori[i].R + tori[i].r)){
-				xs = tori[i].x - (tori[i].R + tori[i].r);
-			}
+		// 	if(xs > tori[i].x - (tori[i].R + tori[i].r)){
+		// 		xs = tori[i].x - (tori[i].R + tori[i].r);
+		// 	}
 
-			if(xl < tori[i].x + (tori[i].R + tori[i].r)){
-				xl = tori[i].x + (tori[i].R + tori[i].r);
-			}
+		// 	if(xl < tori[i].x + (tori[i].R + tori[i].r)){
+		// 		xl = tori[i].x + (tori[i].R + tori[i].r);
+		// 	}
 
-			if(ys > tori[i].y - (tori[i].R + tori[i].r)){
-				ys = tori[i].y - (tori[i].R + tori[i].r);
-			}
+		// 	if(ys > tori[i].y - (tori[i].R + tori[i].r)){
+		// 		ys = tori[i].y - (tori[i].R + tori[i].r);
+		// 	}
 
-			if(yl < tori[i].y + (tori[i].R + tori[i].r)){
-				yl = tori[i].y + (tori[i].R + tori[i].r);
-			}
-		}
+		// 	if(yl < tori[i].y + (tori[i].R + tori[i].r)){
+		// 		yl = tori[i].y + (tori[i].R + tori[i].r);
+		// 	}
+		// }
 
-		long long zd, zm, xd, xm, yd, ym;
-		{
-			zd = (zl - zs)*100000 + 1;
-			zm = zs*100000;
-			xd = (xl - xs)*100000 + 1;
-			xm = xs*100000;
-			yd = (yl - ys)*100000 + 1;
-			ym = ys*100000;
-
-		}
+		// long long zd, zm, xd, xm, yd, ym;
+		// {
+		// 	zd = (zl - zs)*100000 + 1;
+		// 	zm = zs*100000;
+		// 	xd = (xl - xs)*100000 + 1;
+		// 	xm = xs*100000;
+		// 	yd = (yl - ys)*100000 + 1;
+		// 	ym = ys*100000;
+		// }
 
 		srand(time(0));
-		int c = 0;
+		int c = 0, tot = 1000000, acc = 100000000;
+		double prob = 0.0, inv =0.0;
+		double pi = 3.141592654;
+		double alpha, beta, mag;
+		double x, y, z;
 		bool ft = true;
-		for(int i = 0; i < 1000000; i++){
-			ft = false;
-			double xx = (rand()%xd + xm)*0.00001, yy = (rand()%yd + ym)*0.00001, zz = (rand()%zd + zm)*0.00001;
-			// std::cout << xx << " " << yy << " " << zz << "\n"; 
-			for(int j = 0; j < n && !ft; j++){
-				if(cPnt(j, xx, yy, zz, tori)){
+		for(int j = 1; j <= tot; j++){
+			alpha = 2*pi*(((rand()%acc)*1.0)/(acc*1.0));
+			beta = 2*pi*(((rand()%acc)*1.0)/(acc*1.0));
+			mag = tori[i].r*(((rand()%acc)*1.0)/(acc*1.0));
+			x = tori[i].x + tori[i].R*std::cos(alpha) + mag*std::cos(beta)*std::cos(alpha);
+			y = tori[i].y + tori[i].R*std::sin(alpha) + mag*std::cos(beta)*std::sin(alpha);
+			z = tori[i].z + mag*std::sin(beta);
+
+			ft = true;
+			for(int t = 0; t < i && ft; t++){
+				if(cPnt(t, x, y, z, tori)){
 					c++;
-					ft = true;
+					ft = false;
+				}
+			}
+
+			if(j%1000 == 0)
+			{
+				prob += ((j - c)*1.0)/(j*1.0);//*(j);
+				inv += 1.0; //(j);
+				if(i > 0){
+					// std::cout << std::fixed << setprecision(6) << ((j - c)*1.0)/(j*1.0) << "\n";
 				}
 			}
 		}
-
-		vol += ((xl - xs)*(yl - ys)*(zl - zs)/1000000.0)*c;
+		// std::cout << c << std::endl;
+		// prob /= 1000.0;
+		std::cout << std::fixed << std::setprecision(10)<< prob << "\n";
+		vol += (2.0*pi*pi*tori[i].r*tori[i].r*tori[i].R)*(prob/inv);
 		// std::cout << c << "\n";
 	}
 
-	std::cout << vol << "\n";// << ht.size() << "\n";
+	std::cout << std::fixed << std::setprecision(10)<< vol << "\n";
 	return 0;
 }
