fd89357641174b41eb4245d63befce5fbebb5863
deeepeshthakur <deeepeshthakur@gmail.com>
2018-12-13 13:29:42 +0530

    Third Trial

--- CodeChef/DecemberLongChallenge/BeforeHavingDonuts.cpp
+++ CodeChef/DecemberLongChallenge/BeforeHavingDonuts.cpp
@@ -2,10 +2,15 @@
 
 using namespace std;
 double vol = 0.0;
+const double pi = 3.1415926535897932384626;
 
 struct torus{
 	double x, y, z, r, R;
 };
+
+bool compareBV(torus& a, torus& b){
+	return a.r*a.r*a.R > b.r*b.r*b.R;
+}
 
 bool cPnt(int i, double x, double y, double z, std::vector < torus >& tori){
 	if(std::abs(tori[i].z - z) <= tori[i].r){
@@ -16,6 +21,36 @@ bool cPnt(int i, double x, double y, double z, std::vector < torus >& tori){
 		}
 	}
 	return false;
+}
+
+double probCommon(int i, double theta1, double theta2, int k, std::vector < torus >& tori){
+	// srand(time(0));
+	// double pi = 3.141592654;
+	double alpha, beta, mag, prob = 0.0, inv = 0.0, x, y, z;
+	int c = 0, acc = 10000000;
+	bool ft = true;
+	for(int j = 1; j <= k; j++){
+		alpha = theta1 + (theta2 - theta1)*(((rand()%acc)*1.0)/(acc*1.0));
+		beta = pi*(((rand()%acc)*1.0)/(acc*1.0)) - pi;
+		mag = tori[i].r*(((rand()%acc)*1.0)/(acc*1.0));
+		x = tori[i].x + tori[i].R*std::cos(alpha) + mag*std::cos(beta)*std::cos(alpha);
+		y = tori[i].y + tori[i].R*std::sin(alpha) + mag*std::cos(beta)*std::sin(alpha);
+		z = tori[i].z + mag*std::sin(beta);
+
+		ft = true;
+		for(int t = 0; t < i && ft; t++){
+			if(cPnt(t, x, y, z, tori)){
+				c++;
+				ft = false;
+			}
+		}
+
+		if(j == 100 && c == 0){
+			return 1.0;
+		}
+	}
+
+	return ((k - c)*1.0)/(k*1.0);
 }
 
 int main(){
@@ -29,82 +64,23 @@ int main(){
 		std::cin >> tori[i].x >> tori[i].y >> tori[i].z >> tori[i].R >> tori[i].r;
 	}
 
+	std::sort(tori.begin(), tori.end(), compareBV);
+
+	vol = 0.0;
+	srand(time(0));
+	double prob = 0.0;
+	int div = 100000, trials = 1000;
 	for(int i = 0; i < n; i++)
 	{
-		// double xs = 0, xl = 0, ys = 0, yl = 0, zs = 0, zl = 0;
-		// for(int i = 0; i < n; i++){
-		// 	if(zs > tori[i].z - tori[i].r){
-		// 		zs = tori[i].z - tori[i].r;
-		// 	}
-
-		// 	if(zl < tori[i].z + tori[i].r){
-		// 		zl = tori[i].z + tori[i].r;
-		// 	}
-
-		// 	if(xs > tori[i].x - (tori[i].R + tori[i].r)){
-		// 		xs = tori[i].x - (tori[i].R + tori[i].r);
-		// 	}
-
-		// 	if(xl < tori[i].x + (tori[i].R + tori[i].r)){
-		// 		xl = tori[i].x + (tori[i].R + tori[i].r);
-		// 	}
-
-		// 	if(ys > tori[i].y - (tori[i].R + tori[i].r)){
-		// 		ys = tori[i].y - (tori[i].R + tori[i].r);
-		// 	}
-
-		// 	if(yl < tori[i].y + (tori[i].R + tori[i].r)){
-		// 		yl = tori[i].y + (tori[i].R + tori[i].r);
-		// 	}
-		// }
-
-		// long long zd, zm, xd, xm, yd, ym;
-		// {
-		// 	zd = (zl - zs)*100000 + 1;
-		// 	zm = zs*100000;
-		// 	xd = (xl - xs)*100000 + 1;
-		// 	xm = xs*100000;
-		// 	yd = (yl - ys)*100000 + 1;
-		// 	ym = ys*100000;
-		// }
-
-		srand(time(0));
-		int c = 0, tot = 1000000, acc = 100000000;
-		double prob = 0.0, inv =0.0;
-		double pi = 3.141592654;
-		double alpha, beta, mag;
-		double x, y, z;
-		bool ft = true;
-		for(int j = 1; j <= tot; j++){
-			alpha = 2*pi*(((rand()%acc)*1.0)/(acc*1.0));
-			beta = 2*pi*(((rand()%acc)*1.0)/(acc*1.0));
-			mag = tori[i].r*(((rand()%acc)*1.0)/(acc*1.0));
-			x = tori[i].x + tori[i].R*std::cos(alpha) + mag*std::cos(beta)*std::cos(alpha);
-			y = tori[i].y + tori[i].R*std::sin(alpha) + mag*std::cos(beta)*std::sin(alpha);
-			z = tori[i].z + mag*std::sin(beta);
-
-			ft = true;
-			for(int t = 0; t < i && ft; t++){
-				if(cPnt(t, x, y, z, tori)){
-					c++;
-					ft = false;
-				}
-			}
-
-			if(j%1000 == 0)
-			{
-				prob += ((j - c)*1.0)/(j*1.0);//*(j);
-				inv += 1.0; //(j);
-				if(i > 0){
-					// std::cout << std::fixed << setprecision(6) << ((j - c)*1.0)/(j*1.0) << "\n";
-				}
+		double vZero = 2*pi*pi*tori[i].r*tori[i].r*tori[i].R;
+		if(i == 0){
+			vol += vZero;
+		}
+		else{
+			for(int j = 0; j < div; j++){
+				vol += (probCommon(i, (2*pi*j)/(div*1.0), (2*pi*(j + 1))/(div*1.0), trials, tori)*vZero)/(div*1.0);
 			}
 		}
-		// std::cout << c << std::endl;
-		// prob /= 1000.0;
-		std::cout << std::fixed << std::setprecision(10)<< prob << "\n";
-		vol += (2.0*pi*pi*tori[i].r*tori[i].r*tori[i].R)*(prob/inv);
-		// std::cout << c << "\n";
 	}
 
 	std::cout << std::fixed << std::setprecision(10)<< vol << "\n";
